083222307491e96e4ef368583c4d13e740b99729
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-02 19:32:21 -0300

    refactor: add Styled Components to Header Component

--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -1,21 +1,27 @@
 import { BsCart } from 'react-icons/bs'
 
 import './header.styles.css'
+import {
+  HeaderContainer,
+  HeaderItem,
+  HeaderItems,
+  HeaderTitle
+} from './header.styles'
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <h2 className="header-title">CLUB CLOTHING</h2>
-      <div className="header-items">
-        <div className="header-item">Explorar</div>
-        <div className="header-item">Login</div>
-        <div className="header-item">Criar conta</div>
-        <div className="header-item">
+    <HeaderContainer>
+      <HeaderTitle>CLUB CLOTHING</HeaderTitle>
+      <HeaderItems>
+        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem>Login</HeaderItem>
+        <HeaderItem>Criar conta</HeaderItem>
+        <HeaderItem>
           <BsCart size={25} />
           <p style={{ marginLeft: 5 }}>5</p>
-        </div>
-      </div>
-    </div>
+        </HeaderItem>
+      </HeaderItems>
+    </HeaderContainer>
   )
 }
 
