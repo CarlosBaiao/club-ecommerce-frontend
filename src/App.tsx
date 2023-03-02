2d2fda61d3f04a598100d0a3ece9d255f4edf755
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-01 21:24:25 -0300

    feat: change global styles and add Poppind font

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { FunctionComponent } from 'react'
+interface AppProps {
+  message?: string
+}
 
-function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+const App: FunctionComponent<AppProps> = ({ message }) => {
+  return <h1>Hello word</h1>
 }
 
 export default App
