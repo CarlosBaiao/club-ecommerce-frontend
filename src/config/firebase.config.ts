f71d97f8e64f63b35902d77a1d9968a6d58ede3b
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-04 23:29:44 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAztx1C_9EhVHpAvMr4aiv0053IU69EEWo',
+  authDomain: 'club-ecommerce-72056.firebaseapp.com',
+  projectId: 'club-ecommerce-72056',
+  storageBucket: 'club-ecommerce-72056.appspot.com',
+  messagingSenderId: '422747493667',
+  appId: '1:422747493667:web:962aeee37c826a5509a896'
+}
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
