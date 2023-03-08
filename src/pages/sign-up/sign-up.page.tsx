91c5fbfe3659e5ec8a40403e94cb495d1618a156
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-07 22:03:37 -0300

    feat: add user account creation

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -1,6 +1,8 @@
 import { FiLogIn } from 'react-icons/fi'
 import { useForm } from 'react-hook-form'
 import validator from 'validator'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
 
 // Components
 import CustomButton from '../../components/custom-button/custom-button.component'
@@ -16,8 +18,11 @@ import {
   SignUpInputContainer
 } from './sign-up.styles'
 
+// Utilities
+import { auth, db } from '../../config/firebase.config'
+
 interface SignUpForm {
-  name: string
+  firstName: string
   lastName: string
   email: string
   password: string
@@ -34,8 +39,22 @@ const SignUpPage = () => {
 
   const watchPassword = watch('password')
 
-  const handleSubmitPress = (data: SignUpForm) => {
-    console.log(data)
+  const handleSubmitPress = async (data: SignUpForm) => {
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+
+      await addDoc(collection(db, 'users'), {
+        id: userCredentials.user.uid,
+        firstName: data.firstName,
+        email: userCredentials.user.email
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -47,13 +66,13 @@ const SignUpPage = () => {
           <SignUpInputContainer>
             <p>Nome</p>
             <CustomInput
-              hasError={!!errors?.name}
+              hasError={!!errors?.firstName}
               placeholder="Digite seu nome"
-              {...register('name', {
+              {...register('firstName', {
                 required: true
               })}
             />
-            {errors.name?.type === 'required' && (
+            {errors.firstName?.type === 'required' && (
               <InputErrorMessage>O nome é obrigatório.</InputErrorMessage>
             )}
           </SignUpInputContainer>
