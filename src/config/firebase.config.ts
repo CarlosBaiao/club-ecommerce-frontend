ca595a94075f23c6503866233f71326533f525db
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-07 22:03:07 -0300

    chore: initialize firebase auth

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAztx1C_9EhVHpAvMr4aiv0053IU69EEWo',
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const auth = getAuth(app)
