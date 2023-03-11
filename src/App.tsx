52b5c7b02008b6afe26504b6d93dae8bfa4a1454
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-11 13:37:56 -0300

    feat: add Exolore  Page and initial Categories Overview Component

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ import { collection, getDocs, query, where } from 'firebase/firestore'
 import HomePage from './pages/home/home.page'
 import Login from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
+import ExplorePage from './pages/explore/explore.page'
+
+// Components
+import Loading from './components/loading/loading.component'
 
 // Utilities
 import { auth, db } from './config/firebase.config'
 import { UserContext } from './contexts/user.context'
 import { userConverter } from './converters/firestore.converters'
-import Loading from './components/loading/loading.component'
 
 const App: FunctionComponent = () => {
   const [isInitializing, setIsInitializing] = useState(true)
@@ -44,12 +47,13 @@ const App: FunctionComponent = () => {
     return setIsInitializing(false)
   })
 
-  if (isInitializing) return <Loading/>
+  if (isInitializing) return <Loading />
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -31,11 +31,16 @@ const Header = () => {
   const handleLogoClick = () => {
     navigate('/')
   }
+
+  const handleExploreClick = () => {
+    navigate('/explore')
+  }
+
   return (
     <HeaderContainer>
       <HeaderTitle onClick={handleLogoClick}>CLUB CLOTHING</HeaderTitle>
       <HeaderItems>
-        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem onClick={handleExploreClick}>Explorar</HeaderItem>
         {!isAuthenticated && (
           <>
             <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
