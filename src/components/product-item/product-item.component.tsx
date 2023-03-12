649ccf792777ecdcf9f3db00031c3fc62f5cdc1a
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-12 19:27:34 -0300

    feat: add Product Item Component

---
+++ src/components/product-item/product-item.component.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from 'react'
+
+// Styles
+import {
+  ProductContainer,
+  ProductImage,
+  ProductInfo
+} from './product-item.styles'
+
+// Utilities
+import Product from '../../types/product.type'
+
+interface ProductItemProps {
+  product: Product
+}
+
+const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => {
+  return (
+    <ProductContainer>
+      <ProductImage imageUrl={product.imageUrl} />
+      <ProductInfo>
+        <p>{product.name}</p>
+        <p>R${product.price}</p>
+      </ProductInfo>
+    </ProductContainer>
+  )
+}
+
+export default ProductItem
