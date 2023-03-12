80662b772dc4b5427c0c75f997be5e56bdb8b13a
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-12 19:28:03 -0300

    feat: add Loading to Categories Overview

--- src/components/categories-overview/categories-overview.component.tsx
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -8,15 +8,19 @@ import { CategoryContext } from '../../contexts/category.context'
 
 // Components
 import CategoryOverview from '../category-overview/category-overview.component'
+import Loading from '../loading/loading.component'
 
 const CategoriesOverview: FunctionComponent = () => {
-  const { categories, fetchCategories } = useContext(CategoryContext)
+  const { categories, fetchCategories, isLoading } = useContext(CategoryContext)
 
   useEffect(() => {
     if (categories.length === 0) {
       fetchCategories()
     }
   }, [])
+
+  if (isLoading) return <Loading />
+
   return (
     <Container>
       {categories.map((category) => (
