dcd1ef3b1afedd03f4bfd0796dcc06642d24dde0
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-22 20:49:05 -0300

    test: ensure login shows error if email is invalid

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -13,4 +13,21 @@ describe('Login', () => {
     await findByText('O e-mail é obrigatório.')
     getByText('A senha é obrigatória.')
   })
+
+  it('should show error if email is invalid', async () => {
+    const { getByPlaceholderText, findByText, getByText } = renderWithRedux(
+      <LoginPage />,
+      {}
+    )
+
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+
+    userEvent.type(emailInput, 'invalid_email')
+
+    const submitButton = getByText('Entrar')
+
+    userEvent.click(submitButton)
+
+    await findByText('Por favor, insira um e-mail válido.')
+  })
 })
