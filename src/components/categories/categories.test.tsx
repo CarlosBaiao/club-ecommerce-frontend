ab6036fa433a453c98c8b354f22ca51d42569386
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-21 22:37:33 -0300

    refactor: change mockReturnValue to mockImplementation

--- src/components/categories/categories.test.tsx
+++ src/components/categories/categories.test.tsx
@@ -7,7 +7,7 @@ jest.mock('firebase/firestore')
 describe('Categories', () => {
   it('should fetch and show categories', async () => {
     const mockedFirestore = firestore as any
-    mockedFirestore.getDocs.mockReturnValue([
+    mockedFirestore.getDocs.mockImplementation(async () => [
       {
         data() {
           return {
@@ -18,9 +18,9 @@ describe('Categories', () => {
       }
     ])
 
-    mockedFirestore.collection.mockReturnValue({
+    mockedFirestore.collection.mockImplementation(() => ({
       withConverter: () => {}
-    })
+    }))
 
     const { findByText, getByText } = renderWithRedux(<Categories />, {})
 
