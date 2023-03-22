d034baae34b137e3fe613888f9aca94119dbdd19
Carlos Augusto <carlos.correa@owinteractive.com>
2023-03-21 21:34:13 -0300

    test: ensure product item shows correct products

---
+++ src/components/product-item/product-item.test.tsx
@@ -0,0 +1,19 @@
+import { renderWithRedux } from '../../helpers/test.helpers'
+import Product from '../../types/product.type'
+import ProductItem from './product-item.component'
+
+describe('Product Item', () => {
+  it('should show correct product', () => {
+    const product: Product = {
+      id: '1',
+      imageUrl: 'image_url',
+      name: 'Boné',
+      price: 100
+    }
+    const { getByText } = renderWithRedux(<ProductItem product={product} />, {})
+
+    getByText(/boné/i)
+    getByText('R$100')
+    getByText(/adicionar ao carrinho/i)
+  })
+})
